7be1bd383afcb29edc25aad9b82ca17451051c69
Loïc Hamet <1453246+loic-h@users.noreply.github.com>
2024-03-08 17:33:05 +0100

    Style container

--- src/App.tsx
+++ src/App.tsx
@@ -7,40 +7,42 @@ function App() {
   const favorites = designs.filter(item => item.isFavorite);
 
   return (
-    <div className="text-content-primary">
-      <Spacer size="500" />
-      <section>
-        <SectionTitle headline="My favorites" subheadline="Lorem Ipsum dolor sit amet consectetur." />
-        <Spacer size="i500" />
-        <div>
-          {favorites.map(item => (
-            <EditorialCard
-              key={item.id}
-              imageSrc={item.imageSrc}
-              headline={item.title}
-              subheadline={item.subtitle}
-              authorAvatarSrc={item.author.avatarSrc}
-              authorName={item.author.name} />
-          ))}
-        </div>
-        <Spacer size="i600" />
-      </section>
-      <section>
-        <SectionTitle headline="My designs" subheadline="Lorem Ipsum dolor sit amet consectetur." />
-        <Spacer size="i500" />
-        <div className="grid gap-300 grid-cols-3">
-          {designs.map(item => (
-            <EditorialCard
-              key={item.id}
-              imageSrc={item.imageSrc}
-              headline={item.title}
-              subheadline={item.subtitle}
-              authorAvatarSrc={item.author.avatarSrc}
-              authorName={item.author.name} />
-          ))}
-        </div>
-      </section>
-      <Spacer size="500" />
+    <div className=" flex flex-col items-center">
+      <div className="text-content-primary px-500 max-w-[1200px] w-full">
+        <Spacer size="500" />
+        <section>
+          <SectionTitle headline="My favorites" subheadline="Lorem Ipsum dolor sit amet consectetur." />
+          <Spacer size="i500" />
+          <div>
+            {favorites.map(item => (
+              <EditorialCard
+                key={item.id}
+                imageSrc={item.imageSrc}
+                headline={item.title}
+                subheadline={item.subtitle}
+                authorAvatarSrc={item.author.avatarSrc}
+                authorName={item.author.name} />
+            ))}
+          </div>
+          <Spacer size="i600" />
+        </section>
+        <section>
+          <SectionTitle headline="My designs" subheadline="Lorem Ipsum dolor sit amet consectetur." />
+          <Spacer size="i500" />
+          <div className="grid gap-300 grid-cols-3">
+            {designs.map(item => (
+              <EditorialCard
+                key={item.id}
+                imageSrc={item.imageSrc}
+                headline={item.title}
+                subheadline={item.subtitle}
+                authorAvatarSrc={item.author.avatarSrc}
+                authorName={item.author.name} />
+            ))}
+          </div>
+        </section>
+        <Spacer size="500" />
+      </div>
     </div>
   );
 }
