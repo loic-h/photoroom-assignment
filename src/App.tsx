be93a3bdd8cfbb1f862eb920b5533b07dc68d159
Loïc Hamet <1453246+loic-h@users.noreply.github.com>
2024-03-08 15:36:24 +0100

    Display data

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,65 @@
-import { useState } from "react";
-import viteLogo from "/vite.svg";
 import data from "./data.json";
 
 function App() {
-  const [count, setCount] = useState(0);
 
-  console.log({ data });
+  const designs = data.designs;
+  const favorites = designs.filter(item => item.isFavorite);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-      </div>
-      <h1 className="underline">Vite + React</h1>
-      <div>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p>Click on the Vite and React logos to learn more</p>
-    </>
+    <div>
+      <section>
+        <div>
+          <h2>My favorites</h2>
+          <p>Lorem Ipsum dolor sit amet consectetur.</p>
+        </div>
+        <div>
+          {favorites.map(item => (
+            <div key={item.id}>
+              <div>
+                <img src={item.imageSrc} alt="" />
+              </div>
+              <div>
+                <div>
+                  <h4>Category</h4>
+                  <h3>{item.title}</h3>
+                  <p>{item.subtitle}</p>
+                </div>
+                <div>
+                  <img src={item.author.avatarSrc} alt="" />
+                  <h5>{item.author.name}</h5>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
+      <section>
+        <div>
+          <h2>My designs</h2>
+          <p>Lorem Ipsum dolor sit amet consectetur.</p>
+        </div>
+        <div>
+          {designs.map(item => (
+            <div key={item.id}>
+              <div>
+                <img src={item.imageSrc} alt="" />
+              </div>
+              <div>
+                <div>
+                  <h4>Category</h4>
+                  <h3>{item.title}</h3>
+                  <p>{item.subtitle}</p>
+                </div>
+                <div>
+                  <img src={item.author.avatarSrc} alt="" />
+                  <h5>{item.author.name}</h5>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
+    </div>
   );
 }
 
