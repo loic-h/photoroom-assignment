301f8ac5acd16608b93068a30b564e1c0a6bf431
Loïc Hamet <1453246+loic-h@users.noreply.github.com>
2024-03-08 17:27:05 +0100

    Style EditorialCard

--- src/components/EditorialCard/index.tsx
+++ src/components/EditorialCard/index.tsx
@@ -9,18 +9,18 @@ const EditorialCard = ({
 }) => {
 
   return (
-    <div className="flex flex-col">
-      <div>
-        <img src={imageSrc} alt="" />
+    <div className="flex flex-col rounded-500 border-neutral-2 border overflow-hidden">
+      <div className="overflow-hidden h-0 relative" style={{ paddingTop: "56.25%" }}>
+        <img src={imageSrc} alt="" className="block w-full h-full object-contain absolute top-0"/>
       </div>
-      <div>
-        <div>
-          <h4>Category</h4>
-          <h3>{headline}</h3>
-          <p>{subheadline}</p>
+      <div className="p-200 pb-250 flex flex-col gap-200">
+        <div className="flex flex-col gap-100">
+          <h4 className="uppercase text-comp-semi-400 text-content-secondary">Category</h4>
+          <h3 className="text-comp-600">{headline}</h3>
+          <p className="text-comp-600 text-content-secondary">{subheadline}</p>
         </div>
-        <div>
-          <img src={authorAvatarSrc} alt="" />
+        <div className="flex gap-100 items-center text-comp-500 text-content-secondary">
+          <img className="block rounded-full overflow-hidden w-240 h-250 object-contain" src={authorAvatarSrc} alt="" />
           <h5>{authorName}</h5>
         </div>
       </div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,15 +3,21 @@ import plugin from "tailwindcss/plugin";
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  extend: {},
   content: ["./index.html", "./src/**/*.{html,ts,tsx}"],
   theme: {
     colors: {
       content: {
         primary: "#000000",
         secondary: "#707177"
+      },
+      neutral: {
+        2: "#E6E7ED"
       }
     },
     spacing: {
+      0: 0,
+      100: '8px',
       200: '16px',
       250: '20px',
       300: '24px',
@@ -31,11 +37,27 @@ module.exports = {
         lineHeight: "150%",
         fontWeight: "500",
       }],
+      "comp-semi-400": ["12px", {
+        lineHeight: "130%",
+        fontWeight: "600",
+      }],
+      "comp-500": ["14px", {
+        lineHeight: "130%",
+        fontWeight: "500",
+      }],
+      "comp-600": ["17px", {
+        lineHeight: "130%",
+        fontWeight: "500",
+      }],
       "head-1000": ["50px", {
         lineHeight: "110%",
         fontWeight: "700",
         letterSpacing: "-0.5px"
       }]
+    },
+    borderRadius: {
+      full: "99999px",
+      500: "10px",
     }
   },
   plugins: [
